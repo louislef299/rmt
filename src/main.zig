4b7c6d55a02e7358801f3435a8991fac439faa1c
Louis Lefebvre <lefebl4@medtronic.com>
2025-04-23 08:11:16 -0500

    feat: List files in directory

--- src/main.zig
+++ src/main.zig
@@ -1,58 +1,13 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
 const std = @import("std");
-const lib = @import("bake_lib");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const stdout = std.io.getStdOut().writer();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var cwd = try std.fs.cwd().openDir(".", .{ .iterate = true });
+    defer cwd.close();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-
-    var num: i32 = undefined;
-    num = lib.add(1, 1);
-    std.debug.print("Got the following response: {d}\n", .{num});
-
-    // _ in this case leaves the job of finding array length to the compiler
-    const nums = [_]i32{ num, num + 2, num + 4, num + 5 };
-    const new_nums = [_]i32{ 7, 8, 9 };
-    const final_nums = nums ++ new_nums;
-    num = lib.add(nums[2], final_nums[6]);
-    std.debug.print("The new number is {d}\n", .{num});
-
-    const phrase = "I own your" ++ " editor!!!";
-    std.debug.print("{s}\n", .{(phrase ++ " ") ** 3 ++ "3x"});
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    var it = cwd.iterate();
+    while (try it.next()) |entry| {
+        try stdout.print("{s}\n", .{entry.name});
+    }
 }
