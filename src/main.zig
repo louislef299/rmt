ae51486775569827a17e94c64c46bb9b7bdce9da
Louis Lefebvre <lefebl4@medtronic.com>
2025-04-23 14:51:41 -0500

    feat: Add ability to walk the fs

--- src/main.zig
+++ src/main.zig
@@ -10,4 +10,14 @@ pub fn main() !void {
     while (try it.next()) |entry| {
         try stdout.print("{s}\n", .{entry.name});
     }
+
+    std.debug.print("we are now going to try walking through the dir...\n", .{});
+    const allocator = std.heap.page_allocator;
+
+    var walker = try cwd.walk(allocator);
+    defer walker.deinit();
+
+    while (try walker.next()) |entry| {
+        try stdout.print("{s}\n", .{entry.path});
+    }
 }
