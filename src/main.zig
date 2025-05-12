2e358884bc3fc960ff8e86c4fc96d37c827f8c62
Louis Lefebvre <lefebl4@medtronic.com>
2025-05-12 13:57:42 -0500

    fix: General comptime errors

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,68 @@
 const std = @import("std");
-const io = std.io;
-const mem = std.mem;
+
+const OptionError = error{
+    UnknownInput,
+    HelpMsg,
+};
 
 const usage =
     \\Usage: rmt [options]
     \\
     \\General Options:
     \\
-    \\  -h, --help       Print command-specific usage
-    \\  -i, --interactive   interactive output
-    \\  -r, --recursive  Walk filepath starting at current directory
+    \\  -h, --help          Print command-specific usage
+    \\  -i, --interactive   Interactive output
+    \\  -r, --recursive     Walk filepath starting at current directory
     \\
 ;
 
 // Create an Option type, figure out what options are set based on flags, return
 // Options type, and then run the rmt filesystem logic
+
+// when you get to parsing args, just take a look at the process lib:
+// https://ziglang.org/documentation/0.14.0/std/#std.process
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    // Parse command-line arguments
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    const o = Option.init(args[1..]) catch |err| switch (err) {
+        OptionError.HelpMsg => {
+            try printHelp();
+            return;
+        },
+        else => {
+            std.debug.print("issue reading from args: {s}\n", .{args});
+            return err;
+        },
+    };
+
+    var cwd = try std.fs.cwd().openDir(".", .{ .iterate = true });
+    defer cwd.close();
+
+    if (o.recursive) {
+        std.debug.print("we are now going to try walking through the dir...\n", .{});
+        var walker = try cwd.walk(allocator);
+        defer walker.deinit();
+
+        while (try walker.next()) |entry| {
+            std.debug.print("{s}\n", .{entry.path});
+        }
+    } else {
+        std.debug.print("just looking in the current directory...\n", .{});
+        var it = cwd.iterate();
+        while (try it.next()) |entry| {
+            std.debug.print("{s}\n", .{entry.name});
+        }
+    }
+}
+
+fn printHelp() !void {
+    return std.io.getStdErr().writer().writeAll(usage);
+}
 
 // Represents the General Options available.
 pub const Option = struct {
@@ -28,15 +76,14 @@ pub const Option = struct {
         var inter = false;
 
         for (args) |arg| {
-            if (mem.eql(u8, arg, "--recursive") || mem.eql(u8, arg, "-r")) {
+            if (std.mem.eql(u8, arg, "--recursive") or std.mem.eql(u8, arg, "-r")) {
                 r = true;
-            } else if (mem.eql(u8, arg, "--interactive") || mem.eql(u8, arg, "-i")) {
+            } else if (std.mem.eql(u8, arg, "--interactive") or std.mem.eql(u8, arg, "-i")) {
                 inter = true;
-            } else if (mem.eql(u8, arg, "--help")) {
-                return io.getStdOut().writeAll(usage);
+            } else if (std.mem.eql(u8, arg, "--help")) {
+                return OptionError.HelpMsg;
             } else {
-                std.debug.print("unknown option {s}\n", .{arg});
-                return io.getStdOut().writeAll(usage);
+                return OptionError.UnknownInput;
             }
         }
 
@@ -46,35 +93,3 @@ pub const Option = struct {
         };
     }
 };
-
-// when you get to parsing args, just take a look at the process lib:
-// https://ziglang.org/documentation/0.14.0/std/#std.process
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-
-    // Parse command-line arguments
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
-
-    const option = try Option.init(args);
-
-    var cwd = try std.fs.cwd().openDir(".", .{ .iterate = true });
-    defer cwd.close();
-
-    if (option.recursive) {
-        std.debug.print("we are now going to try walking through the dir...\n", .{});
-        var walker = try cwd.walk(allocator);
-        defer walker.deinit();
-
-        while (try walker.next()) |entry| {
-            try std.debug.print("{s}\n", .{entry.path});
-        }
-    } else {
-        std.debug.print("just looking in the current directory...\n", .{});
-        var it = cwd.iterate();
-        while (try it.next()) |entry| {
-            try std.debug.print("{s}\n", .{entry.name});
-        }
-    }
-}
