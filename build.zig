71af85bd92f1265603f89726f0f3dc27f24c1eb1
Louis Lefebvre <louislefebvre1999@gmail.com>
2025-08-06 15:09:32 -0500

    fix: DNRY in build script

--- build.zig
+++ build.zig
@@ -8,86 +8,88 @@ const targets: []const std.Target.Query = &.{
     .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
 };
 
+const version: std.SemanticVersion = .{
+    .major = 0,
+    .minor = 0,
+    .patch = 2,
+};
+
+fn addExeOptions(o: *std.Build.Step.Options, q: std.Target.Query) void {
+    o.addOption([]const u8, "cpu_arch", @tagName(q.cpu_arch orelse unreachable));
+    o.addOption([]const u8, "os", @tagName(q.os_tag orelse unreachable));
+
+    if (q.abi) |a| {
+        const abi_name = @tagName(a);
+        const abi_with_dash = std.fmt.comptimePrint("-{s}", .{abi_name});
+        o.addOption([]const u8, "abi", abi_with_dash);
+    } else {
+        o.addOption([]const u8, "abi", "");
+    }
+    o.addOption([]const u8, "version", std.fmt.comptimePrint("{d}.{d}.{d}", .{ version.major, version.minor, version.patch }));
+}
+
+fn addRunSteps(b: *std.Build, exe: *std.Build.Step.Compile) void {
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    const run_step = b.step("run", "Run rmt");
+    run_step.dependOn(&run_cmd.step);
+}
+
+fn addTestSteps(b: *std.Build, t: std.Build.ResolvedTarget, o: std.builtin.OptimizeMode) void {
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = t,
+        .optimize = o,
+    });
+    linkSLRE(b, exe_unit_tests);
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
+
+fn linkSLRE(b: *std.Build, exe: *std.Build.Step.Compile) void {
+    exe.addIncludePath(b.path("slre"));
+    exe.addCSourceFile(.{ .file = b.path("slre/slre.c") });
+    exe.linkLibC();
+}
+
+fn buildTarget(b: *std.Build, q: std.Target.Query, t: std.Build.ResolvedTarget, o: std.builtin.OptimizeMode, exeName: []const u8) *std.Build.Step.Compile {
+    const options = b.addOptions();
+    const exe = b.addExecutable(.{
+        .name = exeName,
+        .root_source_file = b.path("src/main.zig"),
+        .target = t,
+        .optimize = o,
+    });
+    linkSLRE(b, exe);
+    addExeOptions(options, q);
+
+    exe.root_module.addOptions("build_config", options);
+    b.installArtifact(exe);
+    return exe;
+}
+
 pub fn build(b: *std.Build) void {
-    const version: std.SemanticVersion = .{
-        .major = 0,
-        .minor = 0,
-        .patch = 2,
-    };
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     if (b.option(bool, "release-build", "Build executables for release target architectures") orelse false) {
-        //=> Release Build Flow <=//
         inline for (targets) |t| {
-            const options = b.addOptions();
-            options.addOption([]const u8, "version", std.fmt.comptimePrint("{d}.{d}.{d}", .{ version.major, version.minor, version.patch }));
-
-            const arch = @tagName(t.cpu_arch orelse unreachable);
-            const os = @tagName(t.os_tag orelse unreachable);
-            const abi = if (t.abi) |a| "-" ++ @tagName(a) else "";
-            options.addOption([]const u8, "cpu_arch", arch);
-            options.addOption([]const u8, "os", os);
-            options.addOption([]const u8, "abi", abi);
-
-            const exe = b.addExecutable(.{
-                .name = std.fmt.comptimePrint("rmt-{s}-{s}{s}", .{ arch, os, abi }),
-                .root_source_file = b.path("src/main.zig"),
-                .target = b.resolveTargetQuery(t),
-                .optimize = .ReleaseSafe,
+            const exeName = std.fmt.comptimePrint("rmt-{s}-{s}{s}", .{
+                @tagName(t.cpu_arch orelse unreachable),
+                @tagName(t.os_tag orelse unreachable),
+                if (t.abi) |a| "-" ++ @tagName(a) else "",
             });
-            exe.addIncludePath(b.path("slre"));
-            exe.addCSourceFile(.{ .file = b.path("slre/slre.c") });
-            exe.linkLibC();
-
-            exe.root_module.addOptions("build_config", options);
-            b.installArtifact(exe);
+            _ = buildTarget(b, t, target, .ReleaseSafe, exeName);
         }
     } else {
-        const options = b.addOptions();
-        options.addOption([]const u8, "version", std.fmt.comptimePrint("{d}.{d}.{d}", .{ version.major, version.minor, version.patch }));
-        options.addOption([]const u8, "cpu_arch", "dev");
-        options.addOption([]const u8, "os", "dev");
-        options.addOption([]const u8, "abi", "");
-
-        //=> Normal Build Flow <=//
-        const exe = b.addExecutable(.{
-            .name = "rmt",
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .version = version,
-        });
-
-        exe.addIncludePath(b.path("slre"));
-        exe.addCSourceFile(.{ .file = b.path("slre/slre.c") });
-        exe.linkLibC();
-
-        exe.root_module.addOptions("build_config", options);
-        b.installArtifact(exe);
-
-        //=> Run Steps <=//
-        const run_cmd = b.addRunArtifact(exe);
-        run_cmd.step.dependOn(b.getInstallStep());
-
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-        const run_step = b.step("run", "Run rmt");
-        run_step.dependOn(&run_cmd.step);
-
-        //=> Test Steps <=//
-        const exe_unit_tests = b.addTest(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        exe_unit_tests.addIncludePath(b.path("slre"));
-        exe_unit_tests.addCSourceFile(.{ .file = b.path("slre/slre.c") });
-        exe_unit_tests.linkLibC();
-        const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-        const test_step = b.step("test", "Run unit tests");
-        test_step.dependOn(&run_exe_unit_tests.step);
+        const exe = buildTarget(b, .{}, target, optimize, "rmt");
+        addRunSteps(b, exe);
+        addTestSteps(b, target, optimize);
     }
 }
