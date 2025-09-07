914e2b9e0e5169ee4ff836ec0b37d8e08b2fa6b9
Louis Lefebvre <lefebl4@medtronic.com>
2025-09-07 10:34:29 -0500

    fix: Require v0.14.1 in build process(thanks ghostty)
    related #7

--- build.zig
+++ build.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
+comptime {
+    requireZig("0.14.1");
+}
+
 const targets: []const std.Target.Query = &.{
     .{ .cpu_arch = .aarch64, .os_tag = .macos },
     .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .gnu },
@@ -102,5 +106,21 @@ pub fn build(b: *std.Build) void {
         }, target, .ReleaseSmall, "rmt");
         addRunSteps(b, exe);
         addTestSteps(b, target, optimize);
+    }
+}
+
+/// Require a specific version of Zig to build this project.
+/// https://github.com/ghostty-org/ghostty/blob/main/src/build/zig.zig
+pub fn requireZig(comptime required_zig: []const u8) void {
+    // Fail compilation if the current Zig version doesn't meet requirements.
+    const current_vsn = builtin.zig_version;
+    const required_vsn = std.SemanticVersion.parse(required_zig) catch unreachable;
+    if (current_vsn.major != required_vsn.major or
+        current_vsn.minor != required_vsn.minor)
+    {
+        @compileError(std.fmt.comptimePrint(
+            "Your Zig version v{} does not meet the required build version of v{}",
+            .{ current_vsn, required_vsn },
+        ));
     }
 }
